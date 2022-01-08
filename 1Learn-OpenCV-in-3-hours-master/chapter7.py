a9209ba401b7490617b9b89b1d2105dd673e5ee7
Ritesh Meshram <74177895+RIT-MESH@users.noreply.github.com>
2022-01-09 01:39:02 +0530

    Color Detection

---
+++ 1Learn-OpenCV-in-3-hours-master/chapter7.py
@@ -0,0 +1,98 @@
+import cv2
+import numpy as np
+
+def empty(a):
+    pass
+
+def stackImages(scale,imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        ver = hor
+    return ver
+
+
+
+path = 'Resources/lambo.png'
+
+# create new window and named in trackbar
+cv2.namedWindow("TrackBars")
+cv2.resizeWindow("TrackBars",640,240)
+#Trackbars that will help us play around with the values in real so that we can find the optimum minimum
+# and maximum values of our color
+cv2.createTrackbar("Hue Min","TrackBars",0,179,empty)
+cv2.createTrackbar("Hue Max","TrackBars",19,179,empty)
+#saturation
+cv2.createTrackbar("Sat Min","TrackBars",110,255,empty)
+#saturation
+cv2.createTrackbar("Sat Max","TrackBars",240,255,empty)
+cv2.createTrackbar("Val Min","TrackBars",153,255,empty)
+cv2.createTrackbar("Val Max","TrackBars",255,255,empty)
+#after above step we will get 6 trackbars
+
+while True:
+    img = cv2.imread(path)
+    # convert to HSV
+    imgHSV = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+   # Apply trackbar to image
+    h_min = cv2.getTrackbarPos("Hue Min","TrackBars")
+    h_max = cv2.getTrackbarPos("Hue Max", "TrackBars")
+    s_min = cv2.getTrackbarPos("Sat Min", "TrackBars")
+    s_max = cv2.getTrackbarPos("Sat Max", "TrackBars")
+    v_min = cv2.getTrackbarPos("Val Min", "TrackBars")
+    v_max = cv2.getTrackbarPos("Val Max", "TrackBars")
+
+    print(h_min,h_max,s_min,s_max,v_min,v_max)
+    #lower limit
+    lower = np.array([h_min,s_min,v_min])
+    #upper limit
+    upper = np.array([h_max,s_max,v_max])
+    mask = cv2.inRange(imgHSV,lower,upper)
+
+    # create a new image from mask image
+
+# we are checking these
+# two images the mask and the original
+# image and we are checking wherever we
+# have these white pixels we are getting
+# from this image and creating a
+# completely new image from it.
+
+    imgResult = cv2.bitwise_and(img,img,mask=mask)
+
+
+    # cv2.imshow("Original",img)
+    # cv2.imshow("HSV",imgHSV)
+    # cv2.imshow("Mask", mask)
+    # cv2.imshow("Result", imgResult)
+
+    imgStack = stackImages(0.6,([img,imgHSV],[mask,imgResult]))
+    cv2.imshow("Stacked Images", imgStack)
+
+    cv2.waitKey(1)
+
+
